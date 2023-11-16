58d42329dab56668ca44973154cbe66e6baa163c
minwookkim <minwookkim@naver.com>
2023-11-16 16:18:26 +0900

    model수정11161618

--- final-pjt-back/community/models.py
+++ final-pjt-back/community/models.py
@@ -7,8 +7,11 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    article_like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='user_like_articles'
+    )
 
-class Comment(models.Model):
+class Articlecomment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField(max_length=50)
--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -2,12 +2,25 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
 class Movie(models.Model):
-    like_users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name='like_movies'
+    adult = models.BooleanField()
+    genres = models.ManyToManyField(Genre)
+    id = models.IntegerField()
+    overview = models.TextField()
+    popularity = models.FloatField()
+    poster_path = models.CharField(max_length=200)
+    release_date = models.DateField()
+    title = models.CharField(max_length=50)
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    movie_like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='user_like_movies'
     )
 
-class Comment(models.Model):
+class Moviecomment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.CharField(max_length=20)
