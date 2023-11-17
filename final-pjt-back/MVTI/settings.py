4257d2843e1fc8599294623456c226e94ff4da2b
minwookkim <minwookkim@naver.com>
2023-11-17 15:11:57 +0900

    11171511

--- final-pjt-back/MVTI/settings.py
+++ final-pjt-back/MVTI/settings.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+env = environ.Env(DEBUG=(bool, False))
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+TMDB_API_KEY = env('TMDB_API_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import requests
+import requests, json
 from django.conf import settings
+from .serializers import MovieSerializer
 
 # Create your views here.
+TMDB_URL = 'https://api.themoviedb.org/3/'
+TMDB_API_KEY = settings.TMDB_API_KEY
 
 # api_key로 데이터 받아오기
+@api_view(['GET'])
 def getdata(request):
-    url = "https://api.themoviedb.org/3/movie/popular?api_key=4c5ad12762391c65cbb04605be859ce5&language=ko-KR&page=1"
-    response = requests.get(url).json()
-    print(response)
-    return response
+    movie_list = []
+    for i in range(1, 2):
+        url = f"{TMDB_URL}movie/popular?api_key={TMDB_API_KEY}&language=ko-KR&page={i}"
+        movies = requests.get(url).json()
+
+        for movie in movies['results']:
+            if movie.get('release_date', '') and (movie.get('overview') != '' and movie.get('poster_path') != None and movie.get('release_date') != None):
+                movie = {
+                    'adult': movie['adult'],
+                    'genres': movie['genre_ids'],
+                    'id': movie['id'],
+                    'overview': movie['overview'],
+                    'popularity': movie['popularity'],
+                    'poster_path': movie['poster_path'],
+                    'release_date': movie['release_date'],
+                    'title': movie['title'],
+                    'vote_average': movie['vote_average'],
+                    'vote_count': movie['vote_count'],
+                    'movie_like_users': []
+                }
+                movie_list.append(movie)
+
+    with open('movies.json', 'w', encoding='UTF-8') as m:
+        json.dump(movie_list, m, ensure_ascii=False, indent=4)
+
+    print(movie_list)
+
+    return Response(movie_list)
