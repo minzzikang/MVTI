ffd4c6102070d980eb1822b12235b49b943e4534
kanagyi95 <kanagyi95@gmail.com>
2023-11-17 14:21:56 +0900

    store script 작성

--- final-pjt-front/mvti-service/src/views/Accounts/SignUpView.vue
+++ final-pjt-front/mvti-service/src/views/Accounts/SignUpView.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="d-flex justify-content-center">
         <div class="d-flex w-100 justify-content-center align-items-center">
-            <form class="form-container w-50 bg-white p-4 border rounded">
+            <form @submit.prevent="signUp" class="form-container w-50 bg-white p-4 border rounded">
                 <h4>회원 가입을 환영합니다!</h4>
                 <div class="mt-3">
                     <label for="userId">아이디</label>
-                    <input type="text" name="userId" id="userId" class="form-control mb-3">
+                    <input type="text" name="userId" id="userId" class="form-control mb-3" v-model.trim="userId">
                     <label for="PW">비밀번호</label>
-                    <input type="password" name="PW" id="PW" class="form-control mb-3" placeholder="영문, 숫자, 특문 중 2개 조합 8자 이상">
+                    <input type="password" name="PW" id="PW" class="form-control mb-3" 
+                        placeholder="영문, 숫자, 특문 중 2개 조합 8자 이상" v-model.trim="password">
                     <label for="pwCheck">비밀번호 확인</label>
                     <input type="password" name="pwCheck" id="pwCheck" class="form-control mb-3">
                     <label for="nickname">닉네임</label>
-                    <input type="text" name="nickname" id="nickname" class="form-control mb-3" placeholder="2자 이상">
+                    <input type="text" name="nickname" id="nickname" class="form-control mb-3"
+                        placeholder="2자 이상" v-model.trim="nickname">
                     <label for="age">나이</label>
                     <input type="text" name="age" id="age" class="form-control mb-3">
                     <label for="mbti">본인의 MBTI는? (선택사항)</label>
-                    <input type="text" name="mbti" id="mbti" class="form-control mb-3">
+                    <input type="text" name="mbti" id="mbti" class="form-control mb-3" v-model.trim="mbti">
                     <input type="submit" class="btn btn-primary mt-3" value="가입하기">
                 </div>
             </form>
@@ -28,12 +30,21 @@ import { ref } from 'vue'
 import { useMovieStore } from '@/stores/movie'
 
 const store = useMovieStore()
+
 const userId = ref('')
 const password = ref('')
 const passwordCheck = ref('')
 const nickname = ref('')
 const mbti = ref('')
 
+const signUp = function () {
+    const payload = {
+        userId: userId.value,
+        password: password.value,
+        passwordCheck: passwordCheck.value
+    }
+    store.signUp(payload)
+}
 
 </script>
 
--- final-pjt-front/mvti-service/src/views/Movies/HomeView.vue
+++ final-pjt-front/mvti-service/src/views/Movies/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink } from 'vue-router'
 import { ref } from 'vue'
+import { useMovieStore } from '@/stores/movie'
 
+const store = useMovieStore()
 const userId = ref('')
 const password = ref('')
 
@@ -12,12 +14,14 @@ const password = ref('')
     <div class="background d-flex">
         <div class="container d-flex justify-content-center align-items-center flex-column w-100">
         <div class="card w-25 p-2">
-            <form class="card-body">
-                <input class="form-control mb-2" type="text" placeholder="아이디를 입력하세요.">
-                <input class="form-control mb-2" type="password" placeholder="비밀번호를 입력하세요.">
+            <form @submit.prevent="logIn" class="card-body">
+                <input class="form-control mb-2" type="text" placeholder="아이디를 입력하세요."
+                    v-model.trim="userId">
+                <input class="form-control mb-2" type="password" placeholder="비밀번호를 입력하세요."
+                    v-model.trim="password">
                 <div class="d-flex justify-content-between">
                     <RouterLink :to="{ name: 'signup'}">회원이 아니신가요?</RouterLink>
-                    <button class="btn btn-primary btn-sm">로그인</button>
+                    <input type="submit" value="로그인" class="btn btn-primary btn-sm">
                 </div>
             </form>
         </div>
