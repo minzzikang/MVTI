aa82b8e91f3e5ae8f2288ef0d5dfab9c300e0561
minwookkim <minwookkim@naver.com>
2023-11-20 15:45:38 +0900

    11201545

--- final-pjt-front/mvti-service/src/views/Users/UserView.vue
+++ final-pjt-front/mvti-service/src/views/Users/UserView.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <div>
+            <p style="color: white;">userpage</p>
+        </div>
         <UserLikeList />
         <UserCommentMovieList />
         <UserArticleList />
@@ -12,6 +15,27 @@ import UserLikeList from '@/components/Users/UserLikeList.vue'
 import UserCommentMovieList from '@/components/Users/UserCommentMovieList.vue'
 import UserArticleList from '@/components/Users/UserArticleList.vue'
 import UserCommentArticleList from '@/components/Users/UserCommentArticleList.vue'
+
+import { onMounted } from 'vue'
+import { useMovieStore } from '@/stores/movie'
+import axios from 'axios'
+
+const store = useMovieStore()
+
+onMounted(() => {
+    axios({
+        method: 'get',
+        url: `${store.API_URL}/accounts/user`,
+        headers: {
+        Authorization: `Token ${store.token}`
+        }
+    })
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((err) => console.log(err))
+})
+
 </script>
 
 <style scoped>
