5783b5244448466f794ae71e31091eb2c9002506
minzzikang <kanagyi95@gmail.com>
2023-11-20 21:16:30 +0900

    ing

---
+++ final-pjt-front/mvti-service/src/components/Articles/CommentList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <CommentCard
+            v-for="comment in comments"
+            :key="comment.id"
+            :comment="comment"/>
+    </div>
+</template>
+
+<script setup>
+import CommentCard from './CommentCard.vue'
+
+</script>
+
+<style scoped>
+
+</style>
