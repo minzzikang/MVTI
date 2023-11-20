f7cd6b99792efeadd24fc23d21f95fa8f1e38398
kanagyi95 <kanagyi95@gmail.com>
2023-11-20 13:42:49 +0900

    1120  게시글생성 작업중

--- final-pjt-front/mvti-service/src/views/Articles/ArticleCreateView.vue
+++ final-pjt-front/mvti-service/src/views/Articles/ArticleCreateView.vue
@@ -5,7 +5,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import { useMovieStore } from '@/stores/movie'
+import { useRouter } from 'vue-router'
 
+const store = useMovieStore()
+const router = useRouter()
+
+const title = ref(null)
+const content = ref(null)
+
+const createArticle = function () {
+    axios({
+        method: 'post',
+        url: `${store.API_URL}/api/v1/`
+    })
+}
 </script>
 
 <style scoped>
