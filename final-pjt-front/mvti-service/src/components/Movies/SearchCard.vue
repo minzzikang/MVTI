cb1a6edba0be87a91dd3023d8b03fc7330855fa3
김민욱 <minwookkim@naver.com>
2023-11-22 20:56:27 +0900

    11222056

--- final-pjt-front/mvti-service/src/components/Movies/SearchCard.vue
+++ final-pjt-front/mvti-service/src/components/Movies/SearchCard.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="card">
         {{ movie.title }}
-        <!-- {{ searchList.title }} -->
     </div>
 </template>
 
 <script setup>
 defineProps({
     movie: Object,
-    // searchList: Object
 })
 </script>
 
