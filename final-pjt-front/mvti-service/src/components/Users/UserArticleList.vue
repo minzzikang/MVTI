55fe8f55d3e9e6d08526cb1f1474db9917279041
김민욱 <minwookkim@naver.com>
2023-11-24 00:01:07 +0900

    11240001

--- final-pjt-front/mvti-service/src/components/Users/UserArticleList.vue
+++ final-pjt-front/mvti-service/src/components/Users/UserArticleList.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-        <UserArticleCard />
+        <p>좋아요한 게시글</p>
+        <UserArticleCard
+        v-for="article in userLikeArticle"
+        :key="article.id"
+        :article="article"/>
     </div>
 </template>
 
 <script setup>
 import UserArticleCard from '@/components/Users/UserArticleCard.vue'
+import { useArticleStore } from '@/stores/article'
+import { useUserStore } from '@/stores/user'
+import { computed } from 'vue'
+
+const articleStore = useArticleStore()
+const userStore = useUserStore()
+
+const userLikeArticle = computed(() => {
+    return articleStore.articles.filter(article => {
+        return article.article_like_users.includes(userStore.user.pk);
+    })
+})
+
 </script>
 
 <style scoped>
--- final-pjt-front/mvti-service/src/components/Users/UserCommentMovieList.vue
+++ final-pjt-front/mvti-service/src/components/Users/UserCommentMovieList.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        <UserCommentMovieCard />
+        <p>댓글 쓴 영화</p>
+        <UserCommentMovieCard 
+        v-for="movie in userCommentMovie"
+        :key="movie.id"
+        :movie="movie"/>
     </div>
 </template>
 
 <script setup>
 import UserCommentMovieCard from '@/components/Users/UserCommentMovieCard.vue'
+import { useMovieStore } from '@/stores/movie'
+import { useUserStore } from '@/stores/user'
+import { computed } from 'vue'
+
+const movieStore = useMovieStore()
+const userStore = useUserStore()
+
+const userCommentMovie = computed(() => {
+    return movieStore.movies.filter(movie => {
+        return movie.moviecomment_set.some(comment => comment.id === userStore.user.pk);
+    })
+})
+
+
 </script>
 
 <style scoped>
